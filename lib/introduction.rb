13f7a517cda7e8cbf9582b090918c1a21609247d
Evan Fujita <evanfujita@gmail.com>
2020-11-02 17:44:38 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,7 +12,7 @@ def introduction_with_language(name, language)
 end
 
 def introduction_with_language_optional(name, language = "Ruby")
-  puts "Hello, #{name}! So you're studying #{language}."
+  puts "Hi, my  name is #{name} and I am learning to program in #{language}."
 end
 
 
